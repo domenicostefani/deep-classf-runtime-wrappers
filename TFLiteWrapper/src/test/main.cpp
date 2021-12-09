9bdf48e53f7c3fd2853b0af347ac01fcbba9c011
domenicostefani <domenico.stefani@unitn.it>
2021-12-09 11:57:13 +0100

    almost there

--- TFLiteWrapper/src/test/main.cpp
+++ TFLiteWrapper/src/test/main.cpp
@@ -10,7 +10,7 @@
 #include <chrono>  //TODO: remove
 #include <cstdlib>
 
-#include "classifier-lib.hpp"
+#include "../classifier-lib.hpp"
 
 const std::size_t IN_SIZE = 190,
                   OUT_SIZE = 8;
