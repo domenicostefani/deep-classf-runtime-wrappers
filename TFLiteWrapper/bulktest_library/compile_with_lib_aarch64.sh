d3c48d341412d70ac1469f565e018f4085e93690
gitbot <gitbot@unraid.net>
2022-01-27 17:45:49 +0100

    add bulk test utility

---
+++ TFLiteWrapper/bulktest_library/compile_with_lib_aarch64.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+BASE="/home/cimil-01/Develop/timbreInference"
+TFLITE_LIB="/home/cimil-01/Develop/tflite-build-master20210510/linux_aarch64/lib"
+aarch64-linux-gnu-g++ -g -o bulk_inference_linux_aarch64 -L$BASE/the_compiled_library -I$BASE/the_compiled_library -L$TFLITE_LIB main.cpp -pthread -lliteclassifier -ltensorflow-lite -Wl,--no-as-needed -ldl
+
